ab16fdc9a3adade6d99c7f0fd0eadc40d386b632
christyCCCC <222849717+christyCCCC@users.noreply.github.com>
2025-10-09 16:12:10 -0400

    🚀 修復 GitHub Pages 部署設置

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: '/water-evolution-ark-clone/', // 讓 GitHub Pages 能正確載入檔案
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
