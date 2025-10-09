5315ce6412514711251b659352a3653eb535e3ea
christyCCCC <christy.com.tw@gmail.com>
2025-10-10 04:28:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/water-evolution-ark-clone/', // 讓 GitHub Pages 能正確載入檔案
+  plugins: [react()],
+  base: '/water-evolution-ark-clone/', // 添加這行
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
